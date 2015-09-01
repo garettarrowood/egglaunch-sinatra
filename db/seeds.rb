768b8351962d8090e94c58d83768e3286b6bcc73
garettarrowood <garettarrowood@gmail.com>
2015-09-01 12:12:19 -0400

    migrations, models, and seed data set up correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,16 +14,8 @@ crazytim = Member.create(name: "Tim", email: "quick@quick.org")
 letsdothis.members << crazybob
 imconfused.members << [crazytim, crazyliz]
 
-opinion1 = Rule_vote.create(yes?: true)
-opinion2 = Rule_vote.create(yes?: false)
-opinion3 = Rule_vote.create(yes?: false)
-opinion4 = Rule_vote.create(yes?: true)
-opinion5 = Rule_vote.create(yes?: true)
-
-binding.pry
-
-rule1.rule_votes << opinion1
-rule1.rule_votes << [opinion2, opinion3, opinion4, opinion5]
-team
-
-rule2.rule_votes << [opinion2, opinion3, opinion4]
+opinion1 = Rule_vote.create(yes?: true, rule_id: rule1.id, team_id: letsdothis.id)
+opinion2 = Rule_vote.create(yes?: false, rule_id: rule1.id, team_id: imconfused.id)
+opinion3 = Rule_vote.create(yes?: false, rule_id: rule1.id, team_id: teamegg.id)
+opinion4 = Rule_vote.create(yes?: true, rule_id: rule2.id, team_id: letsdothis.id)
+opinion5 = Rule_vote.create(yes?: true, rule_id: rule2.id, team_id: imconfused.id)
